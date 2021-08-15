b77a14d8f2432298e1f1141bb11ba16e0c61bb8e
Gerson Jean Pierre <iblezya@gmail.com>
2021-08-15 16:49:01 -0500

    Add forms

---
+++ products/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Products
+
+class ProductsForm(forms.ModelForm):
+  class Meta:
+    model = Products
+    fields = (
+      'cod_product',
+      'description',
+      'category',
+      'price',
+      'stock'
+    )
