f4b90b9f8f42f70f70907e4be270e3fe06d76b4f
Gerson Jean Pierre <iblezya@gmail.com>
2021-08-15 16:50:22 -0500

    Add models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+  cod_product = models.IntegerField()
+  description = models.CharField(max_length=150)
+  create      = models.DateTimeField(auto_now_add=True)
+  category    = models.CharField(max_length=150)
+  price       = models.FloatField()
+  stock       = models.IntegerField()
+
+def __str__(self):
+  return self.cod_product
