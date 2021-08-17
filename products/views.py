49fa8a71231f9e1b5e04e44ce136ca56c2467b6c
Gerson Jean Pierre <iblezya@gmail.com>
2021-08-17 18:58:08 -0500

    fix code

--- products/views.py
+++ products/views.py
@@ -23,18 +23,18 @@ def ReadProduct(request):
   return render(request, 'products/productos.html', context)
 
 def UpdateProduct(request, cod_product):
-  product = Products.objects.get(id = cod_product)
+  product = Products.objects.get(cod_product = cod_product)
   if request.method == 'POST':
     form = ProductsForm(request.POST, instance= product)
     if form.is_valid():
       form.save()
-      return redirect('home')
+      return redirect('productos')
   else:
     form = ProductsForm(instance= product)
   context = {'form' : form}
   return render(request, 'products/editar.html', context)
 
 def DeleteProduct(request, cod_product):
-  product = Products.objects.get(id = cod_product)
+  product = Products.objects.get(cod_product = cod_product)
   product.delete()
-  return redirect('home')
+  return redirect('productos')
